34d7d5941c45c187db14b8007f50f6d589b73ece
samarth12 <samarth12@live.com>
2018-10-16 00:24:09 -0700

    added plot

--- Logistic Regression/logistic.py
+++ Logistic Regression/logistic.py
@@ -1,78 +1,76 @@
+import load_dataset
 import numpy as np
-import matplotlib.pyplot as plt
-import load_dataset as ld
 import scipy.sparse
-
-def initialize_weighted_vector(dimensions=1, class_count=1):
-    w = np.ones(shape=(dimensions, class_count))
-    print("Initialized weights:",np.shape(w))
-    return w
-
-def create_onehot_label(Y):
-    a = Y.shape[0]
-    y_sparse = scipy.sparse.csr_matrix((np.ones(a), (Y, np.array(range(a)))))
-    y_sparse = np.array(y_sparse.todense()).T
-    print("ONE HOT LABELS:",np.shape(y_sparse))
-    return y_sparse
+import matplotlib.pyplot as plt
 
 
-def calculate_probability(z):
-    py = (np.exp(z).T / (1 + np.sum(np.exp(z), axis=1))).T
-    print("Probability:", np.shape(py))
-    return py
+def prob_calculator(z):
+    p = (np.exp(z).T / (1 + np.sum(np.exp(z), axis=1))).T
+    #print("Probability:", np.shape(p))
+    return p
 
-def fit(w, x, y):
-    m = x.shape[0]
-    y_label = create_onehot_label(y)
-    z = np.dot(x, w)
-    print("Calculated WX:",np.shape(z))
-    posterior_probability = calculate_probability(z)
-    print("Calculated Z:", np.shape(posterior_probability))
-    gradient = np.dot(x.T, (y_label - posterior_probability))
-    print("Calculated Gradient:", np.shape(gradient))
+def model(w, image, label):
+    z = np.dot(image, w)
+    #print("Calculated WX:",np.shape(z))
+    prob = prob_calculator(z)
+    #print("Calculated Z:", np.shape(prob))
+    gradient = np.dot(image.T, (label - prob))
+    #print("Calculated Gradient:", np.shape(gradient))
     return gradient
 
 
-def test_accuracy(w):
-    test_label, test_img = ld.read(dataset="testing", path="MNIST/")
-    test_img_vector = test_img.reshape(test_img.shape[0], -1) / 255.0
-    print("Test image:", np.shape(test_img_vector))
-    posterior_probability = calculate_probability(np.dot(test_img_vector, w))
-    prediction = np.argmax(posterior_probability, axis=1)
-    print("Prediction:",np.shape(prediction))
-    correct_count = sum(prediction == test_label)
-    total_count = float(len(test_label))
-    accuracy = correct_count / total_count
+def test_accuracy(w,test_image, test_label):
+    #print("Test image:", np.shape(test_image))
+    prob = prob_calculator(np.dot(test_image, w))
+    pred = np.argmax(prob, axis=1)
+    count = 0
+    #print("Prediction:",np.shape(prediction))
+    for i in range(len(pred)):
+        if (pred[i] == test_label[i]):
+            count += 1
+    total = float(len(test_label))
+    accuracy = count / total
     return accuracy
 
 
-def init():
-    train_label, train_img = ld.read(dataset = "training", path="MNIST/")
+def main():
+    path = "/Users/samarth/Desktop/Fall 2018/CSE 575/Assignment 2/MNIST"
+    train_labels, train_images = load_dataset.read("training", path)
+    train_images = np.reshape(train_images, (60000, 784)) / 255.0
 
-    #Converting the input images as 1D vectors and regularizing it
-    train_img_vector = train_img.reshape(train_img.shape[0], -1) / 255.0
-    #print(train_img_vector)
-    #print(len(train_label))
-    #print(train_img_vector.shape[1])
+    #print(len(train_images))
+    test_labels, test_images = load_dataset.read("testing", path)
+    test_images = np.reshape(test_images, (10000, 784)) / 255.0
 
-    w = initialize_weighted_vector(train_img_vector.shape[1], len(np.unique(train_label)))
-    print("Weights", np.shape(w))
-    print("Imageset", np.shape(train_img_vector))
+    no_classes = 10
+    w = np.ones(shape=(train_images.shape[1], no_classes))
+    #print("Initialized weights:", np.shape(w))
+    #print("Weights", np.shape(w))
+    #print("Imageset", np.shape(train_images))
 
     step = 1e-4
-    #for every record, updating the value of weight (1 epoch only)
-    accuracies = []
-    epoch_count = 5
-    for i in range(0, epoch_count):
-        print("epoch:", i)
-        grad = fit(w, train_img_vector, train_label)
-        w = w + grad * step
-        print(test_accuracy(w))
-        accuracies.append(test_accuracy(w) * 100)
+    accuracy_list = []
+    iteration = 10
 
-    plt.plot(accuracies)
-    plt.legend(["Accuracy"])
+    size = train_labels.shape[0]
+    labels_final = scipy.sparse.csr_matrix((np.ones(size), (train_labels, np.array(range(size)))))
+    labels_final = np.array(labels_final.todense()).T
 
+    for i in range(0, iteration):
+        print("Iteration Number:", i)
+        gradient = model(w, train_images, labels_final)
+        w = w + gradient * step
+        if i%5==0:
+            step_accuracy = test_accuracy(w,test_images,test_labels)
+            print("Accuracy calculated after "+ str(i) + " interactions", step_accuracy)
+            accuracy_list.append(step_accuracy * 100)
+
+    plt.plot(accuracy_list, color='g')
+    plt.xlabel("Iterations")
+    plt.ylabel("Accuracy")
+    plt.title("Logisitc Regression Graph")
     plt.show()
 
-init()
+
+if __name__ == '__main__':
+    main()
